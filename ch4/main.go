574672595bbbb40d78ff05bc05be676c8616a91e
weiZhen <likelovec@gmail.com>
2022-03-02 18:18:42 +0800

    update

---
+++ ch4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const filePath = "hello.txt"
+
+func main() {
+
+	fmt.Println(os.Args)
+}
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const filePath = "text.txt"
 
 func main() {
-	fmt.Println("hello world")
+
+	t := of2(1, 2)
+
+	t2 := of3(1, 2, "!23")
+	fmt.Println(t.t1)
+	fmt.Println(t2)
+}
+
+type Tuple2[T1 any, T2 any] struct {
+	t1 T1
+	t2 T2
+}
+
+type Tuple3[T1 any, T2 any, T3 any] struct {
+	t1 T1
+	t2 T2
+	t3 T3
+}
+
+func of2[T1 any, T2 any](t1 T1, t2 T2) Tuple2[T1, T2] {
+	return Tuple2[T1, T2]{
+		t1,
+		t2,
+	}
+}
+
+func of3[T1 any, T2 any, T3 any](t1 T1, t2 T2, t3 T3) Tuple3[T1, T2, T3] {
+	return Tuple3[T1, T2, T3]{
+		t1,
+		t2,
+		t3,
+	}
 }
